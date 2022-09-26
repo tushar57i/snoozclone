1d2df763086ab2ad588cea858c5a4aa1d12e1323
A.H. Tushar <tushar.ux@gmail.com>
2022-09-26 20:49:37 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+// banner part start
+$(function (){
+    $('.banner_main').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay:false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
+// banner part end
+
+// popover for manu start
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+// popover manu end
+
+
+// scroll top button
+
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, '300');
+});
+
+
+
